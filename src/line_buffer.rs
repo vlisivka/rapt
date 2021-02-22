36c7cd28aacf5775fd37ae77a8f13fcf4ae530a7
Volodymyr M. Lisivka <vlisivka@gmail.com>
2021-02-22 06:56:51 +0200

    About 1kb is shaved in LineBuffer by avoid conversion of [u8] to str.

---
+++ src/line_buffer.rs
@@ -0,0 +1,150 @@
+use arduino_uno::prelude::*;
+use avr_hal_generic::usart::{Usart, UsartOps};
+
+const BACKSPACE: u8 = 8u8;
+const DELETE: u8 = 127u8;
+
+/// Simple line buffer, to read a line from USART, with support for
+/// backspace or ^H, to delete last character, and ^R, to bring up previous
+/// line.
+pub struct LineBuffer<'a> {
+    len: usize,
+    line: &'a mut [u8],
+}
+
+impl<'a> LineBuffer<'a> {
+    pub fn new(line: &'a mut [u8]) -> Self {
+        LineBuffer { len: 0, line }
+    }
+
+    /// Read line from serial port into line buffer.
+    pub fn read_line<USART, PD0, PD1, CLOCK>(
+        &mut self,
+        serial: &mut Usart<USART, PD0, PD1, CLOCK>,
+    ) -> usize
+    where
+        USART: UsartOps<PD0, PD1>,
+    {
+        let old_len = self.len;
+        self.clear();
+
+        loop {
+            // Read a byte from the serial connection
+            let b = nb::block!(serial.read()).void_unwrap();
+
+            match b {
+                // Enter - complete input and return the line length
+                b'\r' | b'\n' => {
+                    serial.write_byte(b'\r');
+                    serial.write_byte(b'\n');
+                    return self.len;
+                }
+
+                //  ^H or Backspace - erase last character
+                BACKSPACE | DELETE => {
+                    match self.pop() {
+                        Some(_) => {
+                            serial.write_byte(BACKSPACE); // Return cursor back
+                            serial.write_byte(b' '); // Print space over previous char
+                            serial.write_byte(BACKSPACE); // Return cursor back
+                        }
+                        None => {}
+                    }
+                }
+
+                // ^R - recall. Bring up previous string for editing.
+                18 => {
+                    if self.len == 0 && old_len != 0 {
+                        self.len = old_len;
+                        for c in &self.line[..] {
+                            serial.write_byte(*c)
+                        }
+                    }
+                }
+
+                // All other characters
+                _ => match self.append(b) {
+                    Some(c) => serial.write_byte(c),
+                    None => {}
+                },
+            }
+        }
+    }
+
+    /// Append character to the line. Returns character to print, if any.
+    /// Returns None, if character is not printable or buffer is full.
+    pub fn append(&mut self, c: u8) -> Option<u8> {
+        match c {
+            // Control characters or non-ASCII characters
+            0..=31 | 127..=255 => None,
+
+            // Printable characters
+            b' '..=126 => {
+                if self.len < self.line.len() {
+                    self.line[self.len] = c;
+                    self.len += 1;
+                    Some(c)
+                } else {
+                    None
+                }
+            }
+        }
+    }
+
+    /// Remove last character from line. Returns removed character, if any.
+    pub fn pop(&mut self) -> Option<u8> {
+        if self.len > 0 {
+            self.len -= 1;
+            Some(self.line[self.len])
+        } else {
+            None
+        }
+    }
+
+    /// Clear line buffer.
+    pub fn clear(&mut self) {
+        self.len = 0;
+    }
+
+// Commented out because this version is longer by 20 bytes than explicit version.
+//
+//    /// Return iterator for words (separated by whitespace).
+//    pub fn words(&mut self) -> impl Iterator<Item=&[u8]> + '_ {
+//          self.line[0..self.len]
+//            .split(|byte| byte.is_ascii_whitespace())
+//            .filter(|s| !s.is_empty())
+//    }
+
+    /// Return iterator for words (separated by whitespace).
+    pub fn words<'r>(&'r mut self) -> Words<'r> {
+        Words {
+          iter:
+          self.line[0..self.len]
+            .split(is_whitespace as fn (&u8) -> bool) // Hint compiler that we need function pointer here
+            .filter(is_not_empty)
+        }
+    }
+}
+
+/// Helper function for LineBuffer::words() iterator.
+fn is_whitespace(byte: &u8) -> bool {
+    byte.is_ascii_whitespace()
+}
+
+/// Helper function for LineBuffer::words() iterator.
+fn is_not_empty(bytes: &&[u8]) -> bool {
+    !bytes.is_empty()
+}
+
+/// Helper struct for LineBuffer::words() iterator.
+pub struct Words<'a> {
+    iter: core::iter::Filter<core::slice::Split<'a, u8, fn(&u8) -> bool>, fn(&&[u8]) -> bool>,
+}
+
+impl<'a> Iterator for Words<'a> {
+    type Item = &'a [u8];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@
 #![no_main]
 
 extern crate avr_std_stub;
+mod line_buffer;
+
 use arduino_uno::prelude::*;
 use arduino_uno::Serial;
 use avr_hal_generic::usart::{Usart, UsartOps};
 use ufmt;
 
+use line_buffer::LineBuffer;
+
 const MAX_BUF_LEN: usize = 128;
-static mut LINE_BUFFER: [u8;MAX_BUF_LEN] = [0;MAX_BUF_LEN];
-const BACKSPACE: u8 = 8u8;
-const DELETE: u8 = 127u8;
+static mut LINE_BUFFER: [u8; MAX_BUF_LEN] = [0; MAX_BUF_LEN];
 const HELP: &str = "Rust-Arduino Pin Tester help:\r
 Use Backspace or ^H to erase last character. Use ^R to recall last line.\r
 \r
@@ -22,108 +24,12 @@ w - write value to an output pin\r
 r - read value of an input pin\r
 ";
 
-/// Simple line buffer, to read a line from USART, with support for
-/// backspace or ^H, to delete last character, and ^R, to bring up previous
-/// line.
-struct LineBuffer {
-    len: usize,
-    line: &'static mut [u8],
-}
-
-impl LineBuffer {
-    fn new(line: &'static mut [u8]) -> Self {
-        LineBuffer {
-            len: 0,
-            line,
-        }
-    }
-
-    /// Read line from serial port into line buffer.
-    fn read_line<USART, PD0, PD1, CLOCK>(&mut self, serial: &mut Usart<USART, PD0, PD1, CLOCK>) -> usize
-    where USART: UsartOps<PD0, PD1>
-    {
-        let old_len = self.len;
-        self.clear();
-
-        loop {
-            // Read a byte from the serial connection
-            let b = nb::block!(serial.read()).void_unwrap();
-
-            match b {
-                // Enter - complete input and return the line length
-                b'\r' | b'\n' => {
-                    serial.write_byte(b'\r');
-                    serial.write_byte(b'\n');
-                    return self.len;
-                }
-
-                //  ^H or Backspace - erase last character
-                BACKSPACE | DELETE => {
-                    match self.pop() {
-                        Some(_) => {
-                            serial.write_byte(BACKSPACE); // Return cursor back
-                            serial.write_byte(b' '); // Print space over previous char
-                            serial.write_byte(BACKSPACE); // Return cursor back
-                        }
-                        None => {}
-                    }
-                }
-
-                // ^R - recall. Bring up previous string for editing.
-                18 => {
-                    if self.len == 0 && old_len != 0 {
-                      self.len = old_len;
-                      ufmt::uwrite!(serial, "{}", self.as_str()).void_unwrap();
-                    }
-                }
-
-                // All other characters
-                _ => match self.append(b) {
-                    Some(c) => serial.write_byte(c),
-                    None => {}
-                },
-            }
-        }
-    }
-
-    /// Append character to the line. Returns character to print, if any.
-    /// Returns None, if character is not printable or buffer is full.
-    fn append(&mut self, c: u8) -> Option<u8> {
-        match c {
-            // Control characters or non-ASCII characters
-            0..=31 | 127..=255 => None,
-
-            // Printable characters
-            b' '..=126 => {
-                if self.len < self.line.len() {
-                    self.line[self.len] = c;
-                    self.len += 1;
-                    Some(c)
-                } else {
-                    None
-                }
-            }
-        }
-    }
-
-    /// Remove last character from line. Returns removed character, if any.
-    fn pop(&mut self) -> Option<u8> {
-        if self.len > 0 {
-            self.len -= 1;
-            Some(self.line[self.len])
-        } else {
-            None
-        }
-    }
-
-    // Clear buffer
-    fn clear(&mut self) {
-        self.len = 0;
-    }
-
-    fn as_str(&mut self) -> &mut str {
-        core::str::from_utf8_mut(&mut self.line[0..self.len]).unwrap()
-    }
+/// Print error message to serial.
+pub fn error<USART, PD0, PD1, CLOCK>(serial: &mut Usart<USART, PD0, PD1, CLOCK>, message: &str)
+where
+    USART: UsartOps<PD0, PD1>,
+{
+    ufmt::uwrite!(serial, "\x1B[1;41mERROR:\x1B[0m {}\r\n", message).void_unwrap();
 }
 
 #[arduino_uno::entry]
@@ -147,29 +53,27 @@ fn main() -> ! {
 
     loop {
         // Write prompt message to USART.
-        ufmt::uwrite!(&mut serial, "rapt> ").void_unwrap();
+        ufmt::uwrite!(&mut serial, "\x1B[1mrapt>\x1B[0m ").void_unwrap();
 
         // Read a line from the serial connection
-        if line_buffer.read_line(&mut serial) > 0  {
-            let line = line_buffer.as_str();
-            //*DEBUG*/ufmt::uwrite!(&mut serial, "DBG: Line: \"{}\"\r\n", line).void_unwrap();
-
+        if line_buffer.read_line(&mut serial) > 0 {
             // Execute commands
-            for command in line.split_whitespace() {
-                //*DEBUG*/ufmt::uwrite!(&mut serial, "DBG {}: Command: \"{}\"\r\n", line!(), command).void_unwrap();
-                match &command[0..1] {
-                  "h" => ufmt::uwrite!(&mut serial, "{}", HELP).void_unwrap(),
-                  "r" => {
-                      match &command[1..] {
-                        "0" | "1" => ufmt::uwrite!(&mut serial, "ERROR: Reserved pin for USART: \"{}\".\r\n", command).void_unwrap(),
-                        "2" => ufmt::uwrite!(&mut serial, "p{}{} ", &command[1..], if pins.d2.is_high().unwrap() { "H" } else { "L" }).void_unwrap(),
-                        _ => ufmt::uwrite!(&mut serial, "ERROR: Invalid pin: \"{}\".\r\n", command).void_unwrap(),
-                      }
-                  }, // TODO: turn pin into output pin
-                  _ => ufmt::uwrite!(&mut serial, "ERROR: Unknown command: \"{}\". Use \"h\" for help.\r\n", command).void_unwrap(),
+            for command in line_buffer.words() {
+                match command {
+                    [b'h', ..] => ufmt::uwrite!(&mut serial, "{}", HELP).void_unwrap(),
+                    [b'r', tail @ ..] => match tail {
+                        [b'0'] | [b'1'] => error(&mut serial, "Pin is reserved for USART."),
+                        [b'2'] => ufmt::uwrite!(
+                            &mut serial,
+                            "p2{} ",
+                            if pins.d2.is_high().unwrap() { "H" } else { "L" }
+                        )
+                        .void_unwrap(),
+                        _ => error(&mut serial, "Invalid pin number."),
+                    }, // TODO: turn pin into output pin
+                    _ => error(&mut serial, "Unknown command. Use \"h\" for help."),
                 }
             }
-            ufmt::uwrite!(&mut serial, "\r\nOK\r\n").void_unwrap()
         }
     }
 }
